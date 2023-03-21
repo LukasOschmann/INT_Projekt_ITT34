9783126774134e4b0fb16fa5a22e5152842a914b
Lukas Oschmann <lukasoschmann1310@outlook.com>
2023-03-21 16:32:23 +0100

    Anpassung index.html für Verlibnkung auf mysql.php

---
+++ mysql.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title> Fragehomepage Mysql</title>
+        <meta name="author" content="Benedikt Meissner, Lukas Oschmann, Tony Beyermann">
+        <meta name="keywords" content="HTML, ...">
+        <meta charset="UTF-8">
+        <!-- <link rel="stylesheet" href="css/stylesheet.css"> -->
+    </head>
+    <body>
+        <h1>Fragen zu Mysql</h1>
+
+        <?php 
+
+            echo "Hallo Welt!";
+
+        ?>
+    
+    </body>
+</html>
